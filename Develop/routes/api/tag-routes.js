011a6d91afeac8c406db55f7847c7b361b245ec1
Tina Ly <t.tinaly@hotmail.com>
2021-11-19 08:06:32 -0800

    seeds, routes

---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,95 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+// GET all tags
+router.get('/', (req, res) => {
+  Tag.findAll(
+    {
+      include: {
+        model: Product
+      }
+    }
+  )
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// GET find a single tag by its `id`
+router.get('/:id', (req, res) => {
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product
+    }
+  })
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST create a new tag
+router.post('/', (req, res) => {
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// PUT update a tag
+router.put('/:id', (req, res) => {
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(tagData => {
+      if (!tagData) {
+        res.status(404).json({ message: 'Tag Not Found' });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// DELETE a tag by its `id` value
+router.delete('/:id', (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(tagData => {
+      if (!tagData) {
+        res.status(404).json({ message: 'Tag Not Found' });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ecommerce-backend
+<h1>E-Commerce Backend</h1>
+
+<h2>Description</h2>
+This app is the backend portion of an E-Commerce website. Express.js was used for the server and MySQL for the database along with Sequelize as the ORM to run SQL models and queries. The SQL database includes tables for products, categories, tags, and product tags. RESTful API routes are used to make requests and updates from the database which are joined through Sequelize queries.
+
